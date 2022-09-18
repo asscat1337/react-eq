54570d32c20987b584d2236f55bf4f01b41b6b0f
asscat1337 <ruslan.xaziaxmetov@mail.ru>
2022-09-18 15:01:13 +0500

    Получение данных по комнатам

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,61 +1,67 @@
 import { find } from "./services/serviceUser/serviceUser.service";
 import { update } from "./services/ticket/ticket.service";
-import {io} from './server'
+import { io } from "./server";
 import { Socket } from "socket.io";
+import { rooms } from "./utils/rooms";
 
-const connection = new Set()
+const connection = new Set();
 
 
-const socket=async ()=>io.on('connection',(sock:Socket)=>{
-    console.log(`connected ${sock.id}`);
-    connection.add(sock)
-    sock.on("joinRoom", async (room:any) => {
-      sock.join(room);
-      console.log(room)
-    });
-  sock.on("createTicket", async (data:any) => {
-    const findCurrent = await find(data.service_id);
-    findCurrent.map(async (curr)=>{
-      if(curr.service_id === data.service_id){
-         sock.broadcast.to(`ackt${curr.user_id}`).emit("getTicket", data);
-      }
-    })
+const socket = async () => io.on("connection", (sock: Socket) => {
+  console.log(`connected ${sock.id}`);
+  connection.add(sock);
+  sock.on("joinRoom", async (room: any) => {
+    sock.join(room);
   });
-  sock.on("showTv", async (data:any) => {
+  sock.on("createTicket", async (data: any) => {
+    const findCurrent = await find(data.service_id);
+    const { service } = data;
+    const { terminal } = service;
+    findCurrent.map(async (curr) => {
+      if (curr.service_id === data.service_id) {
+        const toRooms = rooms(terminal.name, curr.user_id);
+        sock.broadcast.to(toRooms).emit("getTicket", data);
+      }
+    });
+  });
+  sock.on("showTv", async (data: any) => {
     await update({ isCall: 1 }, data.ticket_id);
-    const {service} = data
-    const {terminal} = service
+    const { service } = data;
+    const { terminal } = service;
 
     sock.to(terminal.name).emit("translateTv", data);
   });
-  sock.on("complete", (data:any) => {
-    console.log(data)
-    io.in("ackt").emit("completeTv", data);
+  sock.on("complete", (data: any) => {
+    const { service } = data;
+    const { terminal } = service;
+    io.in(terminal.name).emit("completeTv", data);
   });
-  sock.on("completeTransfer",(data:any)=>{
-    io.in("ackt").emit("prepareTransfer",data)
-  })
-  sock.on("transferTicket", async(data:any) => {
-    console.log(data)
+  sock.on("completeTransfer", (data: any) => {
+    const { service } = data;
+    const { terminal } = service;
+    io.in(terminal.name).emit("prepareTransfer", data);
+  });
+  sock.on("transferTicket", async (data: any) => {
     const sendObject = data.sendNotice && {
-      notice:data.notice
-    }
-    const {service} = data
-    const {terminal} = service
+      notice: data.notice
+    };
+    const { service } = data;
+    const { terminal } = service;
+    const currentRoom = rooms(terminal.name, data.transferNumber);
     await update({
-      user_id:data.transferNumber,
-      isCall:0,
+      user_id: data.transferNumber,
+      isCall: 0,
       ...sendObject
-    },data.ticket_id)
-    io.in(`${terminal.name}${data.transferNumber}`).emit("transferData",data)
+    }, data.ticket_id);
+    io.in(currentRoom).emit("transferData", data);
   });
 
-  sock.on('disconnect',async (  )=>{
-    console.log(`socket ${sock.id} disconnected`)
-    connection.delete(sock)
-    sock.disconnect(true)
-  })
-})
+  sock.on("disconnect", async () => {
+    console.log(`socket ${sock.id} disconnected`);
+    connection.delete(sock);
+    sock.disconnect(true);
+  });
+});
 
 export {
   socket
---
+++ backend/src/utils/rooms.ts
@@ -0,0 +1,7 @@
+
+
+
+const rooms=(name:string,number:number)=>{
+  return `${name}${number}`
+}
+export {rooms}
